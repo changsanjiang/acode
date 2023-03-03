7807d69fa952ee357c859e075ce0d80d80ecc227
changsanjiang <changsanjiang@gmail.com>
2023-03-03 21:58:59 +0800

    initial

---
+++ app/src/main/java/io/github/changsanjiang/acode/demo/MainActivity.kt
@@ -0,0 +1,8 @@
+package io.github.changsanjiang.acode.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import io.github.changsanjiang.acode.R
+
+class MainActivity: AppCompatActivity(R.layout.main_activity) {
+
+}
---
+++ app/src/main/java/io/github/changsanjiang/acode/recyclerview/TestClass.kt
@@ -0,0 +1,7 @@
+package io.github.changsanjiang.acode.recyclerview
+
+class TestClass {
+    fun test() {
+        println("test fun!")
+    }
+}
